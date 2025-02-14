78828cc50d27860ac1e2f9d4cfb47aa21f95b7ac
jeong724 <oliveyou724@gmail.com>
2025-02-14 16:39:28 +0900

    fix : fix message and code number

--- src/main/java/com/prography/assignment/common/code/BusinessErrorCode.java
+++ src/main/java/com/prography/assignment/common/code/BusinessErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum BusinessErrorCode implements ErrorCode{
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+    BAD_REQUEST(HttpStatus.CREATED, "불가능한 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
