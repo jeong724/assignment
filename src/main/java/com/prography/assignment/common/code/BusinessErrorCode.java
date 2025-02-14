c76918df278d44c19c2c073596be5aee19ceb86a
jeong724 <oliveyou724@gmail.com>
2025-02-14 16:36:15 +0900

    add : add business error code

---
+++ src/main/java/com/prography/assignment/common/code/BusinessErrorCode.java
@@ -0,0 +1,16 @@
+package com.prography.assignment.common.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum BusinessErrorCode implements ErrorCode{
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
