d6ceacc00223ae87263b6c26bac70f5847e5871d
jeong724 <oliveyou724@gmail.com>
2025-02-15 19:11:57 +0900

    add: faker api response dto

--- src/main/java/com/prography/assignment/api/common/init/service/response/FakerGetResponse.java
+++ src/main/java/com/prography/assignment/api/common/init/service/response/FakerGetResponse.java
@@ -1,6 +1,16 @@
 package com.prography.assignment.api.common.init.service.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record FakerGetResponse(
 
+        @JsonProperty(value = "id")
+        Integer fakerId,
+
+        @JsonProperty(value = "username")
+        String name,
+
+        @JsonProperty(value = "email")
+        String email
 ) {
 }
