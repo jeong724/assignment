314fb9fa5e2595fda00c1ce61725ede6b05b9c9a
jeong724 <oliveyou724@gmail.com>
2025-02-20 18:55:43 +0900

    remove : remove dto validation

--- src/main/java/com/prography/assignment/api/room/controller/request/RoomPostRequest.java
+++ src/main/java/com/prography/assignment/api/room/controller/request/RoomPostRequest.java
@@ -1,13 +1,8 @@
 package com.prography.assignment.api.room.controller.request;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record RoomPostRequest(
-        @NotBlank
         int userId,
-        @NotBlank
         String roomType,
-        @NotBlank
         String title
 ) {
 }
